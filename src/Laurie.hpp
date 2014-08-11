dc58d78c52ee28b71229023ff861bbc4e48934f4
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2014-08-11 23:36:18 +0100

    r_konrad coded.

--- src/Laurie.hpp
+++ src/Laurie.hpp
@@ -11,7 +11,7 @@ namespace gauss_konrad {
 		typedef typename Eigen::Matrix<RealType,Eigen::Dynamic,1>  VectorType;
 		typedef typename VectorType::Index IndexType;
 
-		static void r_jacobi(const RealType a,const RealType b,VectorType & alpha, VectorType & beta)
+		static void r_jacobi(const IndexType N,const RealType a,const RealType b,VectorType & alpha, VectorType & beta)
 		{
 			assert(a > -1);
 			assert(b > -1);
@@ -19,8 +19,10 @@ namespace gauss_konrad {
 			assert(alpha.rows()==beta.rows());
 			assert(alpha.rows() > 0);
 
-			const IndexType N=alpha.rows();
-			
+			assert(N>0)
+			assert(alpha.rows() > floor(3*N/2))
+			assert(beta.rows() > ceil(3*N/2))
+
 			alpha(0) = (b-a)/(a+b+2.);
 			beta(0) = pow(2.,(a+b+1.))*gamma(a+1.)*gamma(b+1.)/gamma(a+b+2.);
 
@@ -32,7 +34,7 @@ namespace gauss_konrad {
 			}
 		}
 
-		static void r_jacobi_01(const RealType a,const RealType b,VectorType & alpha, VectorType & beta)
+		static void r_jacobi_01(const IndexType N,const RealType a,const RealType b,VectorType & alpha, VectorType & beta)
 		{
 			assert(a > -1);
 			assert(b > -1);
@@ -40,9 +42,11 @@ namespace gauss_konrad {
 			assert(alpha.rows()==beta.rows());
 			assert(alpha.rows() > 0);
 
-			const IndexType N=alpha.rows();
+			assert(N>0)
+			assert(alpha.rows() > floor(3*N/2))
+			assert(beta.rows() > ceil(3*N/2))
 
-			r_jacobi( a, b, alpha, beta);
+			r_jacobi(N, a, b, alpha, beta);
 
 			for(IndexType n=0;n<N;++n)
 			{
@@ -57,60 +61,71 @@ namespace gauss_konrad {
 			}
 
 		}
-/*
-function ab=r_kronrod(N,ab0)
-if length(ab0)<ceil(3*N/2)+1, error('array ab0 too short'), end
-a=mp(zeros(2*N+1,1)); b=a;						% P.H.
-k=0:floor(3*N/2); a(k+1)=ab0(k+1,1);
-k=0:ceil(3*N/2); b(k+1)=ab0(k+1,2);
-s=mp(zeros(floor(N/2)+2,1)); t=s; t(2)=b(N+2);	% P.H.
-for m=0:N-2,
-  k=floor((m+1)/2):-1:0; l=m-k;
-  s(k+2)=cumsum((a(k+N+2)-a(l+1)).*t(k+2)+b(k+N+2).*s(k+1)-b(l+1).*s(k+2));
-  swap=s; s=t; t=swap;
-end
-j=floor(N/2):-1:0; s(j+2)=s(j+1);
-for m=N-1:2*N-3,
-  k=m+1-N:floor((m-1)/2); l=m-k; j=N-1-l;
-  s(j+2)=cumsum(-(a(k+N+2)-a(l+1)).*t(j+2)-b(k+N+2).*s(j+2)+b(l+1).*s(j+3));
-  j=j(length(j)); k=floor((m+1)/2);
-  if rem(m,2)==0, a(k+N+2)=a(k+1)+(s(j+2)-b(k+N+2)*s(j+3))/t(j+3);
-  else b(k+N+2)=s(j+2)/s(j+3);
-  end
-  swap=s; s=t; t=swap;
-end
-a(2*N+1)=a(N)-b(2*N+1)*s(2)/t(2);
-ab=[a b];
-*/
-		static void r_kronrod(const IndexType N,VectorType & alpha, VectorType & beta)
+
+		static void r_kronrod(const IndexType N,VectorType & a, VectorType & b)
 		{
-			assert(alpha.rows()==beta.rows());
-			assert(alpha.rows() > 0);
-
-			assert(alpha.rows() > ceil(3*N/2+1) );
-
-			VectorType a=VectorType::Zero(2*N+1);
-			VectorType b=VectorType::Zero(2*N+1);
-
-			for(IndexType k=0;k<floor(3*N/2);++k)
-			{
-				a(k) = alpha(k)
-			}
-
-			for(IndexType k=0;k<ceil(3*N/2);++k)
-			{
-				b(k) =  beta(k)
-			}
+			assert(a.rows()==b.rows());
+			assert(a.rows() > 0);
+			assert(N>0);
+			assert(2*N+1 == a.rows());
 
 			VectorType s=VectorType::Zero(floor(N/2)+2);
 			VectorType t=VectorType::Zero(floor(N/2)+2);
 
-			t(1)=b(N+1) //t(2)=b(N+2)
+			t(1)=b(N+1);
 
-			for(IndexType m=0;m<N-2;++m)
+			for(IndexType m=0;m<=N-2;++m)
 			{
-				k=floor((m+1)/2):-1:0; l=m-k;
+				RealType u=0;
+				for(IndexType k=floor((m+1)/2);k>=0;--k)
+				{
+					k += 1;
+					IndexType l=m-k;
+					u += ( a(k+N+1) - a(l) )*t(k) + b(k+N+1)*s(k-1) - b(l)*s(k);
+					s(k) = u;
+				}
+
+				VectorType swap(s);
+				s=t;
+				t=swap;
 			}
+
+			for(IndexType j=floor(N/2);j>=0;--j)
+			{
+				j += 1;
+				s(j) = s(j-1);
+			}
+
+			for(IndexType m=N-1;m<=2*N-3;++m)
+			{
+				RealType u=0;
+				IndexType j;
+				for(IndexType k=m+1-N;k<=floor((m-1)/2);++k)
+				{
+					k += 1;
+					IndexType l = m-k;
+					j = N-1-l;
+					u -= ( a(k+N+1) -  a(l) )*t(j) - b(k+N+1)*s(j) + b(l)*s(j+1);
+					s(j) = u;
+				}
+
+				if(m % 2 == 0)
+				{
+					k = m/2 + 1;
+					a(k+N+1) = a(k) + ( s(j) -b(k+N+1)*s(j+1) )/t(j+1);
+				}
+				else
+				{
+					k = (m+1)/2 + 1;
+					b(k+N+1) =  s(j)/s(j+1);
+				}
+
+				VectorType swap(s);
+				s=t;
+				t=swap;
+			}
+
+			a(2*N) = a(N-1) - b(2*N)*s(1)/t(1);
 		}
 
 		
